a384065673c9992fbc5b214d07870d9a41d269b2
ayubsh <ibrahimayubsheikh@gmail.com>
2024-04-02 03:46:56 +0200

    Added - username to User model
    modified the user model to contain github username

--- auth/src/models/User.ts
+++ auth/src/models/User.ts
@@ -4,11 +4,13 @@ import {Document, Schema, model} from "mongoose";
 export type UserDocument = Document & {
   githubId: String,
   repo_url: String
+  username: String
 }
 
 const userSchema = new Schema<UserDocument>({
   githubId: {type: String, required: true},
-  repo_url: {type: String, required: true}
+  repo_url: {type: String, required: true},
+  username: {type: String, required: true}
 })
 
 
